ee34855e024a88a610e623179bd1e2a968315594
AswinVmm <aswina002@gmail.com>
2026-06-01 12:20:07 +0530

    finished

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,18 +4,6 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
 import "./index.css";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-supabase.auth.onAuthStateChange((event, session) => {
-  if (event === "SIGNED_IN") {
-    window.location.href = "/dashboard";
-  }
-});
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
